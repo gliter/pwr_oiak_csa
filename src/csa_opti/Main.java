66db1d4fd9485f7cf374b015df83f0a98fdfed3f
gliter <grzegorz.liter@gmail.com>
2014-05-27 18:18:06 +0200

    backup

--- src/csa_opti/Main.java
+++ src/csa_opti/Main.java
@@ -1,7 +1,6 @@
 package csa_opti;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 	Main() {
@@ -10,9 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 		//Tablica optymalizatorów
 		ArrayList<AOptimizer> optiArr = new ArrayList<AOptimizer>();
-		optiArr.add(new OptiMax());
-		//optiArr.add(new OptiWallace());
-		optiArr.add(new Opti1(2));
+		//optiArr.add(new OptiMax());
+		optiArr.add(new OptiWallace());
+		//optiArr.add(new Opti1(10));
 		
 		ArrayList< TestCase > testCases = new ArrayList< TestCase >();
 		testCases.add(TestCasesFactory.createEvenTestCase(40, 20));
@@ -30,8 +29,8 @@ public class Main {
 				System.out.println("Opt: " + op.getName());
 				op.init(new ArrayList<Integer>(testCase.csaLevel), testCase.width);
 				System.out.println("TC: name: " + testCase.name + ", width: " + testCase.width + ", height: " + testCase.height);
-				//op.runAndPrint();
-				op.run();
+				op.runAndPrint();
+				//op.run();
 				System.out.println("A: " + op.getSumA() + ", T: " + op.getSumT());
 			}
 		}
